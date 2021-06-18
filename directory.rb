2cedca8e4c3ebcb7d4cfad20419ca24076ac00fd
jamescondon <jecondon66@icloud.com>
2021-06-18 17:36:59 +0100

    Names beginning 'N' 12 letters or less

--- directory.rb
+++ directory.rb
@@ -22,9 +22,12 @@ def print_header
 end
 
 def print(students)
-	# modify to print a number before each student name
+  # modify to print a number before each student name
+  # only students whose name begins with "N" and is shorter than 12 characters
   students.each_with_index do |student, index|
-  puts "#{index + 1}. #{student[0]} (#{student[1].capitalize} cohort)"
+    if student[0].chars[0] == "N" && student[0].length < 13
+    puts "#{index + 1}. #{student[0]} (#{student[1].capitalize} cohort)"
+    end
   end
 end
 
